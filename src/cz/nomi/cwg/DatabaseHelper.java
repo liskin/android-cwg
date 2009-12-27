93ac60875bc671984dc104895527f1ea17d7f449
Ondrej Novy <novy@ondrej.org>
2009-12-27 13:39:30 +0000

    GPL3 license.
    git-svn-id: file:///tmp/svn/android-cwg/trunk@3 5ce807ad-b57b-0410-bd59-ceefd8d8fa96

--- src/cz/nomi/cwg/DatabaseHelper.java
+++ src/cz/nomi/cwg/DatabaseHelper.java
@@ -1,3 +1,19 @@
+/*
+This file is part of Android-CWG.
+
+Android-CWG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Android-CWG is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Android-CWG.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package cz.nomi.cwg;
 
 import android.content.Context;
@@ -5,24 +21,24 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "cwg";
-    private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "cwg";
+	private static final int DATABASE_VERSION = 1;
 
-    DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+	DatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE title (_id INTEGER PRIMARY KEY," +
-                "title VARCHAR NOT NULL COLLATE LOCALIZED);");
-        db.execSQL("CREATE TABLE cwg (_id INTEGER PRIMARY KEY," +
-                "title_id INT NOT NULL," +
-                "version INT NOT NULL," +
-                "count INT NOT NULL);");
-    }
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE title (_id INTEGER PRIMARY KEY,"
+				+ "title VARCHAR NOT NULL COLLATE LOCALIZED);");
+		db.execSQL("CREATE TABLE cwg (_id INTEGER PRIMARY KEY,"
+				+ "title_id INT NOT NULL,"
+				+ "version INT NOT NULL,"
+				+ "count INT NOT NULL);");
+	}
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    }
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	}
 }
--- src/cz/nomi/cwg/Import.java
+++ src/cz/nomi/cwg/Import.java
@@ -1,17 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+	This file is part of Android-CWG.
+
+	Android-CWG is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Android-CWG is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Android-CWG.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 package cz.nomi.cwg;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-/**
- *
- * @author onovy
- */
 public abstract class Import {
 	protected InputStream input;
 
@@ -20,5 +28,4 @@ public abstract class Import {
 	}
 
 	public abstract void importData(DatabaseAdapter db) throws IOException;
-
 }
